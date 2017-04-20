87d6e067ea2424d341b81bd365200fe74675a5a4
Simon <sima@chetverukhin.ru>
2017-04-20 16:17:58 +0300

    it's almost done

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,29 @@
 var $ = require('jquery');
 var selectize = require('selectize');
-var Inputmask = require('inputmask');
+var FastClick = require('fastclick');
+var insdpu = require('jquery-mask-plugin');
 
+FastClick.attach(document.body);
 
 $(document).ready(function() {
 
-    $('.row__card').hide();
+    $('.row__card, .row__new-payment, .row__new-credit-date').hide();
+    // $('.input--sum').focus();
+    // $('.input--sum').trigger('touchstart');
+    // $( ".input__desc" ).addClass( "input__desc--active" );
 
     $('input[type=radio][name=from]').change(function(event) {
         $('.row__schet, .row__card').toggle();
         $('.input--card .input-text').focus();
+    });
+
+    $('input[type=radio][name=what-change]').change(function(event) {
+        $('.row__credit-end, .row__new-payment, .row__new-credit-date').toggle();
+        $('.input--sum').focus();
+    });
+
+    $('input[type=radio][name=what-change-mobile]').change(function(event) {
+        $('.row__credit-end, .row__new-payment, .row__new-credit-date').toggle();
     });
 
     $('input[type=radio][name=from-mobile]').change(function(event) {
@@ -20,6 +34,21 @@ $(document).ready(function() {
         $( this ).next( ".input__desc" ).addClass( "input__desc--active" );
     });
 
+    $('.button-submit').click(function () {
+        var submitDisabled = $(this).hasClass('button--disabled');
+        if (!submitDisabled) {
+            $("html, body").animate({scrollTop: 0}, 250);
+            $('.popup').addClass('popup--active');
+            $('.overlay').addClass('overlay--active');
+            $('.input--sms').focus();
+        }
+    })
+
+    $('.overlay, .popup__close').click(function () {
+        $('.popup').removeClass('popup--active');
+        $('.overlay').removeClass('overlay--active');
+    })
+
     $( ".input-text" ).blur(function() {
         var inputText = $(this).val();
         if (inputText.length == 0) {
@@ -27,12 +56,32 @@ $(document).ready(function() {
         }
     });
 
-    var cardMask = new Inputmask("9999 9999 9999 9999", { placeholder: " ", autoclear: false });
-    var dateMask = new Inputmask("99/99", { placeholder: " ", autoclear: false });
-    var cvcMask = new Inputmask("999", { placeholder: " ", autoclear: false });
-    cardMask.mask('.input--card .input-text');
-    dateMask.mask('.input--date .input-text');
-    cvcMask.mask('.input--cvc .input-text');
+    $(".input--sum").keyup(function () {
+        var letters = $(this).val().length;
+        if (letters > 0) {
+            $('.button-submit').removeClass('button--disabled')
+        } else if (letters == 0) {
+            $('.button-submit').addClass('button--disabled')
+        }
+    })
+
+    $(".input--sms").keyup(function () {
+        var letters = $(this).val().length;
+        if (letters == 4) {
+            window.location.href = 'notification.html';
+        }
+    })
+
+    $('.notification').addClass('notification--active');
+    $('.notification__close').click(function () {
+        $('.notification').removeClass('notification--active');
+    })
+
+    $('.input--card .input-text').mask('0000 0000 0000 0000');
+    $('.input--date .input-text').mask('00/00');
+    $('.input--cvc .input-text').mask('000');
+    $('.input--sms').mask('0000');
+    $('.input--sum').mask('0 000 000 000 000 000', {reverse: true});
 
 	$('.select').selectize({
 		valueField: 'id',
@@ -62,11 +111,4 @@ $(document).ready(function() {
 			}
 		}
 	});
-
-    $('body').click(function () {
-            document.getElementById('example-modal').visible = true;
-    })
-
-
-
 });

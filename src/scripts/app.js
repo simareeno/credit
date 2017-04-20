a658a80dd8d7577ea11d3c63eb3fe353253d5956
Simon <sima@chetverukhin.ru>
2017-04-20 17:57:56 +0300

    real data begins

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -78,7 +78,21 @@ $(document).ready(function() {
     })
 
     $('.input--card .input-text').mask('0000 0000 0000 0000');
-    $('.input--date .input-text').mask('00/00');
+    $('.input--date .input-text').mask('01/23', {translation: {
+        '0': {
+            pattern: /[1-3]/, optional: false
+        },
+        '1': {
+            pattern: /[1-9]/, optional: false
+        },
+        '2': {
+            pattern: /[0-9]/, optional: false
+        },
+        '3': {
+            pattern: /[1-9]/, optional: false
+        }
+    }
+});
     $('.input--cvc .input-text').mask('000');
     $('.input--sms').mask('0000');
     $('.input--sum').mask('0 000 000 000 000 000', {reverse: true});
@@ -112,3 +126,71 @@ $(document).ready(function() {
 		}
 	});
 });
+
+var storage = localStorage;
+var userData = {
+    'name': 'Дмитрий Маслов',
+    'accountNumber': '4081 7810 7044 4000 1693',
+    'creditNextPaymentDate': '25 сентября',
+    'creditNextPayment': 6253,
+    'creditDebt': 1882838,
+    'creditLimit': 70000,
+    'creditRecieveDate': '29 сентября 2016',
+    'creditLastPaymentDate': '29 сентября 2049',
+    'creditMonthPayment': 6253,
+    'creditRate': '18,99',
+    'creditDurationMonths': 60,
+    'accounts' : [
+        {
+            'accoutNumber' : 4576,
+            'accountSum' : 62328,
+            'accountCurrent' : true,
+            'accountCards' : 2
+        }, {
+            'accoutNumber' : 2191,
+            'accountSum' : 1923328,
+            'accountCurrent' : true,
+            'accountCards' : 1
+        }
+    ]
+};
+
+if (storage.length == 0) {
+    storage.setItem('user', JSON.stringify(userData));
+}
+
+var data = JSON.parse(storage.getItem('user'))
+console.log(data.creditNextPaymentDate);
+
+$('.creditNextPaymentDate').text(data.creditNextPaymentDate);
+$('.creditNextPayment').text(numberWithSpaces(data.creditNextPayment));
+$('.creditDebt').text(numberWithSpaces(data.creditDebt));
+$('.creditLimit').text(numberWithSpaces(data.creditLimit));
+$('.creditMonthPayment').text(numberWithSpaces(data.creditMonthPayment));
+$('.accountNumber').text(cc_format(data.accountNumber));
+$('.creditRate').text(data.creditRate);
+$('.creditDuration').text(data.creditDuration);
+$('.creditDurationMonths').text(data.creditDurationMonths);
+$('.creditRecieveDate').text(data.creditRecieveDate);
+$('.creditLastPaymentDate').text(data.creditLastPaymentDate);
+
+function numberWithSpaces(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+
+// function cc_format(value) {
+//     var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
+//     var matches = v.match(/\d{4,16}/g);
+//     var match = matches && matches[0] || ''
+//     var parts = []
+//     for (i=0, len=match.length; i<len; i+=4) {
+//         parts.push(match.substring(i, i+4))
+//     }
+//     if (parts.length) {
+//         return parts.join(' ')
+//     } else {
+//         return value
+//     }
+//     }
+// }
